f85717c109ef2e4907fb863991aea72aa8344951
KaoTD <kaotd99@gmail.com>
2018-04-04 09:50:20 +0000

    lineage-15.1 bring up & use me repos

--- shinano.mk
+++ shinano.mk
@@ -1,7 +1,7 @@
 # Copyright (C) 2013 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
+# you may not use this file except in complie with the License.
 # You may obtain a copy of the License at
 #
 #      http://www.apache.org/licenses/LICENSE-2.0
@@ -104,10 +104,6 @@ PRODUCT_COPY_FILES += \
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     e2fsck
-
-#ANC
-PRODUCT_PACKAGES += \
-   ANCQuickSettings
 
 # FM
 PRODUCT_PACKAGES += \
